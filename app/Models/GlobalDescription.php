7a875664887e14e0dd84dd6e4546914e6e0e4a23
Bobby Januario <bobby.januario@student.umn.ac.id>
2024-04-30 14:55:34 +0700

    Add admin page and migration

---
+++ app/Models/GlobalDescription.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalDescription extends Model
+{
+    protected $table = 'global_descriptions';
+
+    protected $fillable = [
+        'year',
+        'month',
+        'week',
+        'description',
+    ];
+
+    public $timestamps = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->year = $model->year ?? date('Y');
+            $model->month = $model->month ?? date('n');
+            $model->week = $model->week ?? ceil((date('j') + date('N')) / 7);
+        });
+    }
+}
+
---
+++ app/Models/MachineData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MachineData extends Model
+{
+    use HasFactory;
+    protected $table = 'machine_data';
+
+    protected $fillable = [
+        'machine_id',
+        'machine_name',
+        'year',
+        'month',
+        'week',
+        'date',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->year = $model->year ?? date('Y');
+            $model->month = $model->month ?? date('n');
+            $model->week = $model->week ?? ceil((date('j') + date('N')) / 7);
+            $model->date = $model->date ?? now();
+        });
+    }
+}
