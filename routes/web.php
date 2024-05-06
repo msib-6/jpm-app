1b19ff438a9d6d17845d19c98841493c75208982
robilions <130848931+robilions@users.noreply.github.com>
2024-05-06 11:46:56 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\MachineController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,31 +13,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Default landing page
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-//Show logged in dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//Middleware route to edit, update, and delete one's profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Show Guest Dashboard
-//Route::get('/dashboardGuest', function () {
-//    return view('guest.dashboardGuest');
-//});
-
-//Route::get('/machineoperation', [MachineController::class, 'showAllMachineOperation']);
-
-Route::get('/dashboardGuest', [MachineController::class, 'showAllMachineOperation'])->name('guest.dashboardGuest');
-
+Route::get('/reset', function () {
+    return view('auth.reset-password');
+});
 
 require __DIR__.'/auth.php';
