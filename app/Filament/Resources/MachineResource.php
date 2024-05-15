6b6825bc4a83aa5c7367c2ce78c4a0bcc595c933
Bobby Januario <bobby.januario@student.umn.ac.id>
2024-05-15 15:20:37 +0700

    Add machine admin

---
+++ app/Filament/Resources/MachineResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MachineResource\Pages;
+use App\Filament\Resources\MachineResource\RelationManagers;
+use App\Models\Machine;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MachineResource extends Resource
+{
+    protected static ?string $model = Machine::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->sortable(),
+                TextColumn::make('machine_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('category')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('line')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMachines::route('/'),
+            'create' => Pages\CreateMachine::route('/create'),
+            'edit' => Pages\EditMachine::route('/{record}/edit'),
+        ];
+    }
+}
