390b83580533c7a7bc0e4c75527fb63be2c8a4fb
robilions <130848931+robilions@users.noreply.github.com>
2024-05-17 16:01:12 +0700

    fix forgot pw

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,17 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="background-login">
+      <div class="wrp">
+            <div class="wrapper">
+            <div class='logo'></div>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -12,14 +25,15 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full input-box" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="button1">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+    </body>
+</html>
