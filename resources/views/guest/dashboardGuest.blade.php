3984a0268da559880d0c1f6a4d0e87d2b85ac979
robilions <130848931+robilions@users.noreply.github.com>
2024-05-25 20:31:56 +0700

    guest dashboard keren

--- resources/views/guest/dashboardGuest.blade.php
+++ resources/views/guest/dashboardGuest.blade.php
@@ -1,4 +1,4 @@
-    <!doctype html>
+<!doctype html>
     <html>
     <head>
         <meta charset="utf-8">
@@ -6,89 +6,103 @@
         @vite('resources/css/guest/dashboard.css')
     </head>
     <body>
-    <div class="dashboard-container">
+    <div class="card h-full">
+        <!--begin::Body-->
+        <div class="card-body py-0">
+            <!--begin::Row-->   
+            <div class="flex items-center h-100">
+                 <!--begin::Col-->
+                <div class="w-7/12 xl:pl-10 pr-2">
 
-        <!-- Bagian 1 -->
-        <div id="bagian-1" class="big-card-container flex items-center justify-center h-screen">
-            <div class="big-card text-white p-6 rounded-lg flex flex-col items-start">
-                <div class="text-container flex-grow">
-                    <h3 class="text-4xl mb-8 font-bold">Welcome To JPM View (Guest Mode)</h3>
-                    <p class="text-lg mb-8">Selamat datang di halaman dashboard eksklusif JPM! Di sini, Anda akan menemukan rangkuman yang komprehensif dan terperinci mengenai aktivitas operasional yang vital bagi kesuksesan bisnis Anda. Melalui visualisasi data yang intuitif dan informatif, Anda dapat dengan mudah melacak kinerja operasional, menganalisis tren, dan mengidentifikasi potensi area perbaikan.</p>
-                </div>
-                <div class="logo-container self-end">
-                    <a href="#" id="pindah-ke-bagian-2" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-center inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                        Pilih Line
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div>
+                                <!-- Bagian 1 -->
+                                <div id="bagian-1" class="section">
+                                <div class="text-container text-white">
+                                <h3 class="text-4xl mb-8 font-bold">Welcome To JPM View (Guest Mode)</h3>
+                                <p class="text-lg mb-8">Selamat datang di halaman dashboard eksklusif JPM! Di sini, Anda akan menemukan rangkuman yang komprehensif dan terperinci mengenai aktivitas operasional yang vital bagi kesuksesan bisnis Anda. Melalui visualisasi data yang intuitif dan informatif, Anda dapat dengan mudah melacak kinerja operasional, menganalisis tren, dan mengidentifikasi potensi area perbaikan.</p>
+                                <div class="buttons">
+                                    <a href="#" id="pindah-ke-bagian-2" class="text-white bg-[#9D6EFF] hover-custom focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-center inline-flex items-center dark:bg-purple-600 hover:bg-purple-600 dark:focus:ring-purple-800">
+                                    Pilih Line
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                                </a>
+                                <a href="/summary" class="text-white bg-[#9D6EFF] hover-custom focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-center inline-flex items-center dark:bg-purple-600 hover:bg-purple-600 dark:focus:ring-purple-800">
+                                    History
+                                </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <!-- Bagian 2 -->
+                        <div id="bagian-2" class="section hidden">
+                            <div class="text-white p-6">
+                                <h3 class="text-3xl mb-8 font-bold">PILIH LINE</h3>
+                                <div id="line-container">
+                                    <!-- Line buttons will be populated here -->
+                                </div>
+                                <div class="logo-container self-start">
+                                    <a href="#bagian-1" id="kembali-ke-bagian-1" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                        </svg>
+                                        Back
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+
+                                <!-- Bagian 3 (Choose Year) -->
+                                <div id="bagian-3" class="section hidden">
+                            <div class="text-white p-6">
+                                <h3 class="text-3xl mb-8 font-bold">PILIH YEAR</h3>
+                                <div id="year-container">
+                                    <!-- Line buttons will be populated here -->
+                                </div>
+                                <div class="logo-container self-start">
+                                    <a href="#bagian-3" id="kembali-ke-bagian-1" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                        </svg>
+                                        Back
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                            
+
+                                <!-- Bagian 4 (Choose Month) -->
+                                <div id="bagian-4" class="section hidden">
+                            <div class="text-white p-6">
+                                <h3 class="text-3xl mb-8 font-bold">PILIH MONTH</h3>
+                                <div id="month-container">
+                                    <!-- Line buttons will be populated here -->
+                                </div>
+                                <div class="logo-container self-start">
+                                    <a href="#bagian-3" id="kembali-ke-bagian-1" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                        </svg>
+                                        Back
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                </div>     
+                        <!--end::Col-->
+                    <!--begin::Col-->
+                <div class=" w-7/12 pt-5 lg:pt-15">
+                <!--begin::Illustration-->
+                <div class="image bg-no-repeat bg-contain bg-right-bottom" style="background-image:url('{{ asset('svg/8.svg') }}'); height: 48em; width: 48em;">
+   </div>
+                <!--begin::Illustration-->
+              </div>
+              <!--end::Col--> 
             </div>
         </div>
-
-        <!-- Bagian 2 (Choose Line) -->
-        <div id="bagian-2" class="big-card-container flex items-center justify-center h-screen hidden">
-            <div class="big-card dark:bg-blue-900 text-white p-6 rounded-lg flex flex-col items-center justify-center">
-                <div class="text-container flex-grow">
-                    <h3 class="text-3xl mb-8 font-bold">PILIH LINE</h3>
-                    <!-- Line -->
-                    <div class="custom-card" id="line-container">
-                        <!-- Line buttons will be populated here -->
-                    </div>
-                </div>
-                <div class="logo-container self-start">
-                    <!-- Tetapkan tombol kembali ke Bagian 1 -->
-                    <a href="#bagian-1" id="kembali-ke-bagian-1" class="text-white button-kembali-1 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                        Back
-                    </a>
-                </div>
-            </div>
+        </div>
+            <!--end::Row--> 
         </div>
 
-        <!-- Bagian 3 (Choose Year) -->
-        <div id="bagian-3" class="big-card-container flex items-center justify-center h-screen hidden">
-            <div class="big-card dark:bg-blue-900 text-white p-6 rounded-lg flex flex-col items-start">
-                <div class="text-container flex-grow">
-                    <h3 class="text-3xl mb-8 font-bold">PILIH YEAR</h3>
-                    <!-- Year -->
-                    <div id="year-container" class="custom-card">
-                        <!-- Year buttons will be populated here -->
-                    </div>
-                </div>
-                <div class="logo-container self-start flex justify-between">
-                    <a href="#bagian-2" id="kembali-ke-bagian-2" class="text-white button-kembali-1 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                        Back
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <!-- Bagian 4 (Choose Month) -->
-        <div id="bagian-4" class="big-card-container flex items-center justify-center h-screen hidden">
-            <div class="big-card dark:bg-blue-900 text-white p-6 rounded-lg flex flex-col items-start">
-                <div class="text-container flex-grow">
-                    <h3 class="text-3xl mb-8 font-bold">PILIH MONTH</h3>
-                    <!-- Month -->
-                    <div id="month-container" class="custom-card">
-                        <!-- Month buttons will be populated here -->
-                    </div>
-                </div>
-                <div class="logo-container self-start flex justify-between">
-                    <a href="#bagian-3" id="kembali-ke-bagian-3" class="text-white button-kembali-1 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-12 py-2.5 text-left inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                        <svg class="rotate-180 w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                        Back
-                    </a>
-                </div>
-            </div>
-        </div>
 
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script>
