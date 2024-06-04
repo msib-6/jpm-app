4056bf92449f0a0a0e015f32a7e6a1abb0f56322
robilions <130848931+robilions@users.noreply.github.com>
2024-06-04 09:51:01 +0700

    guest,pjl bread fix

--- resources/views/guest/dashboardGuest.blade.php
+++ resources/views/guest/dashboardGuest.blade.php
@@ -14,6 +14,44 @@
             <!-- Bagian 1 -->
             <div class="left-section" id="bagian-1">
                 <div class="text-container">
+                    <!-- Breadcrumb -->
+                    <nav class="flex mb-4" aria-label="Breadcrumb">
+                        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                            <li class="inline-flex items-center">
+                                <a href="/guest/dashboard" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                    <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                                    </svg>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li id="breadcrumb-1" style="display: none;">
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Line</a>
+                                </div>
+                            </li>
+                            <li id="breadcrumb-2" style="display: none;">
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Year</a>
+                                </div>
+                            </li>
+                            <li id="breadcrumb-3" style="display: none;">
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Month</a>
+                                </div>
+                            </li>
+                        </ol>
+                    </nav>
+                    <!-- Breadcrumb -->
                     <h3 class="text-4xl mb-8 font-bold">Welcome To JPM View (Guest Mode)</h3>
                     <p class="text-lg mb-8">Selamat datang di halaman dashboard eksklusif JPM! Di sini, Anda akan menemukan rangkuman yang komprehensif dan terperinci mengenai aktivitas operasional yang vital bagi kesuksesan bisnis Anda. Melalui visualisasi data yang intuitif dan informatif, Anda dapat dengan mudah melacak kinerja operasional, menganalisis tren, dan mengidentifikasi potensi area perbaikan.</p>
                     <button type="button" id="pindah-ke-bagian-2" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 inline-flex items-center">
@@ -35,8 +73,9 @@
             <div class="right-section">
                 <!-- Bagian 2 -->
                 <div id="bagian-2" class="hidden">
+                    
                     <div class="p-6">
-                        <h3 class="text-3xl mb-8 font-bold">PILIH LINE</h3>
+                        <h3 class="text-4xl mb-8 font-bold">LINE</h3>
                         <div id="line-container" class="scrollable-container">
                             <!-- Line buttons will be populated here -->
                         </div>
@@ -54,7 +93,38 @@
                 <!-- Bagian 3 -->
                 <div id="bagian-3" class="hidden">
                     <div class="p-6">
-                        <h3 class="text-3xl mb-8 font-bold">PILIH YEAR</h3>
+                    <!-- Breadcrumb
+                    <nav class="flex" aria-label="Breadcrumb">
+                    <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                                <li class="inline-flex items-center">
+                                    <a href="/guest/dashboard" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                                        </svg>
+                                        Dashboard
+                                    </a>
+                                </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                                        Line</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                                        Year</a>
+                                </div>
+                            </li>
+                        </ol>
+                    </nav> -->
+                        <h3 class="text-4xl mb-8 font-bold">YEAR</h3>
                         <div id="year-container" class="scrollable-container">
                             <!-- Year buttons will be populated here -->
                         </div>
@@ -72,7 +142,47 @@
                 <!-- Bagian 4 -->
                 <div id="bagian-4" class="hidden">
                     <div class="p-6">
-                        <h3 class="text-3xl mb-8 font-bold">PILIH MONTH</h3>
+                    <!-- Breadcrumb
+                    <nav class="flex" aria-label="Breadcrumb">
+                    <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                                <li class="inline-flex items-center">
+                                    <a href="/guest/dashboard" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                                        </svg>
+                                        Dashboard
+                                    </a>
+                                </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                                        Line</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                                        Year</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="/guest/dashboard" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                                        Month</a>
+                                </div>
+                            </li>
+                        </ol>
+                    </nav> -->
+                        <h3 class="text-4xl mb-2 font-bold">MONTH</h3>
                         <div id="month-container" class="scrollable-container">
                             <!-- Month buttons will be populated here -->
                         </div>
@@ -112,6 +222,7 @@
                 document.getElementById('bagian-4').classList.add('hidden');
                 document.getElementById('pindah-ke-bagian-2').classList.add('hidden');
                 fetchLines();
+                updateBreadcrumb();
                 });
 
                 // Event listeners for back buttons
@@ -166,6 +277,7 @@
                     selectedLine = line;
                     document.getElementById('bagian-2').classList.add('hidden');
                     document.getElementById('bagian-3').classList.remove('hidden');
+                    document.getElementById('breadcrumb-2').style.display = 'flex'; // Tampilkan breadcrumb "Year"
                     fetchYears(line);
                 }
 
@@ -199,6 +311,7 @@
                     selectedYear = year;
                     document.getElementById('bagian-3').classList.add('hidden');
                     document.getElementById('bagian-4').classList.remove('hidden');
+                    document.getElementById('breadcrumb-3').style.display = 'flex'; // Tampilkan breadcrumb "Month"
                     fetchMonths(selectedLine, year);
                 }
 
@@ -221,15 +334,44 @@
                     months.forEach(month => {
                         const button = document.createElement('button');
                         button.textContent = monthName(month);
-                        button.className = 'btn w-48 h-16 text-lg ml-4 mb-4 text-left';
+                        button.className = 'btn w-48 h-10 text-xl ml-4 mb-4 text-left';
                         button.addEventListener('click', () => {
                             window.location.href = `/guest/viewGuest?line=${selectedLine}&year=${selectedYear}&month=${month}`;
                         });
                         monthContainer.appendChild(button);
                     });
                 }
+               
+
             });
         </script>
+        <script>
+    document.getElementById('pindah-ke-bagian-2').addEventListener('click', function() {
+        document.getElementById('breadcrumb-1').style.display = 'flex';
+        document.getElementById('bagian-2').classList.remove('hidden');
+    });
+
+    document.getElementById('kembali-ke-bagian-1').addEventListener('click', function() {
+        document.getElementById('breadcrumb-1').style.display = 'none';
+        document.getElementById('bagian-2').classList.add('hidden');
+        document.getElementById('breadcrumb-2').style.display = 'none'; // Also hide breadcrumb-2 when going back
+     });
+     document.getElementById('kembali-ke-bagian-2').addEventListener('click', function() {
+        document.getElementById('breadcrumb-2').style.display = 'none';
+        document.getElementById('bagian-3').classList.add('hidden');
+        document.getElementById('breadcrumb-3').style.display = 'none'; // Also hide breadcrumb-2 when going back
+     });
+     document.getElementById('kembali-ke-bagian-3').addEventListener('click', function() {
+        document.getElementById('breadcrumb-3').style.display = 'none';
+        document.getElementById('bagian-4').classList.add('hidden');
+        document.getElementById('breadcrumb-4').style.display = 'none'; // Also hide breadcrumb-2 when going back
+     });
+
+
+
+
+</script>
+
         <script>
   console.clear();
 </script>
