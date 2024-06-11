39764c97cc86f630d56d0dd48772b3ec4cc949c4
robilions <130848931+robilions@users.noreply.github.com>
2024-06-11 17:35:28 +0700

    mgr

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -12,10 +12,8 @@
 
 @section('content')
 <div class="container mx-auto px-4">
-    <div class='card flex justify-between'>
-        <h1 class="text-left text-4xl font-bold text-gray-800">Manager</h1>
-    </div>
-    <div class='weeks-container'>
+    <div class='card my-4 mx-auto flex justify-between items-center shadow-2xl rounded-3xl p-6' style="width: 91.666667%;">
+        <h1 class="text-4xl font-bold text-gray-800">Manager</h1>
         <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
             <ul class="flex flex-wrap -mb-px text-sm font-medium text-center mt-4" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">
                 <li class="me-2 mx-2" role="presentation">
