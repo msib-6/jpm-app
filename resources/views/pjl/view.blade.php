8110eebddeb4d7b93a1b7198468e694f33706abf
Joshua Hotama <joshuahh554@gmail.com>
2024-07-03 12:15:14 +0700

    up

--- resources/views/pjl/view.blade.php
+++ resources/views/pjl/view.blade.php
@@ -1241,8 +1241,8 @@
                                     const nextWeekMachines = await response.json();
                                     const nextWeekMachine = nextWeekMachines.find(m => m.machine_name === machine.machine_name);
                                     if (nextWeekMachine) {
-                                        currentMachineIdWeekly = nextWeekMachine.id;
-                                        currentDay = 1;
+                                        currentMachineIdWeekly = nextWeekMachine.machine_id;
+                                        currentDay = day;
                                     }
                                 }
 
