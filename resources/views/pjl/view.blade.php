3e5c4b8b37ea693b06ec3c1bea87fca477716994
ranggawijayaf <ranggawijaya587@gmail.com>
2024-07-29 10:04:56 +0700

    push view

--- resources/views/pjl/view.blade.php
+++ resources/views/pjl/view.blade.php
@@ -1096,6 +1096,10 @@
                     sendWeekButton.style.display = 'none';
                     openModalButton.style.display = 'none';
                 }
+                if (week === "1") {
+                    sendWeekButton.style.display = 'none';
+                    openModalButton.style.display = 'none';
+                }
             }
 
             async function fetchRevisionNumber(line, year, month, week) {
@@ -1227,22 +1231,20 @@
                     let machineIdKey;
 
                     if (week === 1 && (operation.week === 1 || operation.week === 5 || operation.week ===
-                        6)) {
+                            6)) {
                         machineIdKey = operation.machine_id;
-                    } else if (week === 2 && (operation.week === 1 || operation.week === 2 || operation
-                        .week === 3)) {
+                    } else if (week === 2 && (operation.week === 5 || operation.week === 6 || operation
+                            .week === 2 || operation.week === 3)) {
                         machineIdKey = operation.machine_id;
-                    } else if (week === 3 && (operation.week === 2 || operation.week === 3 || operation
-                        .week === 4)) {
+                    } else if (week === 3 && (operation.week === 3 || operation.week === 4)) {
                         machineIdKey = operation.machine_id;
-                    } else if (week === 4 && (operation.week === 3 || operation.week === 4 || operation
-                        .week === 5)) {
+                    } else if (week === 4 && (operation.week === 4 || operation.week === 5)) {
                         machineIdKey = operation.machine_id;
-                    } else if (week === 5 && (operation.week === 4 || operation.week === 5 || operation
-                        .week === 6)) {
+                    } else if (week === 5 && (operation.week === 5 || operation.week === 6 || operation
+                            .week === 1)) {
                         machineIdKey = operation.machine_id;
                     } else if (week === 6 && (operation.week === 5 || operation.week === 6 || operation
-                        .week === 1)) {
+                            .week === 1)) {
                         machineIdKey = operation.machine_id;
                     } else {
                         machineIdKey = operation.machine_id_parent;
@@ -1367,78 +1369,88 @@
                         }
                     });
 
-                    for (let i = 1; i <= 8; i++) {
-                        const headerDate = document.getElementById(`day${i}`).children[1].textContent
-                            .trim();
-                        const dateParts = headerDate.split(' ');
-                        const day = parseInt(dateParts[0]);
-                        const dayColumn = document.getElementById(`daydata${machine.machine_id}-${day}`);
-                        if (dayColumn) {
-                            const addButton = document.createElement('button');
-                            addButton.className =
-                                'add-jpm-button add-data-button rounded-full bg-grey-500 text-white text-xs w-7 h-7 thin-plus';
-                            addButton.textContent = '+';
-                            addButton.onclick = async function() {
-                                const params = new URLSearchParams(window.location.search);
-                                const line = params.get('line');
-                                currentMachineId = machine.id;
-                                currentMachineIdWeekly = machine.machine_id;
-                                currentDay = day;
-                                currentMonth = getMonthNumber(dateParts[1]);
-                                currentYear = parseInt(dateParts[2]);
+                    function getWeeksInMonth(year, month) {
+    const firstDayOfMonth = new Date(year, month - 1, 1).getDay();
+    const lastDateOfMonth = new Date(year, month, 0).getDate();
+    const daysInMonth = firstDayOfMonth + lastDateOfMonth;
 
-                                const isLastMonday = i === 8;
-                                if (isLastMonday) {
-                                    let nextWeekUrl = '';
-                                    switch (parseInt(week)) {
-                                        case 1:
-                                            nextWeekUrl =
-                                                `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=2`;
-                                            break;
-                                        case 2:
-                                            nextWeekUrl =
-                                                `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=3`;
-                                            break;
-                                        case 3:
-                                            nextWeekUrl =
-                                                `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=4`;
-                                            break;
-                                        case 4:
-                                            nextWeekUrl =
-                                                `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=5`;
-                                            break;
-                                        case 5:
-                                            nextWeekUrl =
-                                                `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=6`;
-                                            break;
-                                        default:
-                                            console.error('Invalid week number');
-                                            return;
-                                    }
+    return Math.ceil(daysInMonth / 7);
+}
 
-                                    const response = await fetch(nextWeekUrl);
-                                    const nextWeekMachines = await response.json();
-                                    const nextWeekMachine = nextWeekMachines.find(m => m
-                                        .machine_name === machine.machine_name);
-                                    if (nextWeekMachine) {
-                                        currentMachineIdWeekly = nextWeekMachine.machine_id;
-                                        currentMachineId = nextWeekMachine
-                                            .id; // Menggunakan ID dari mesin minggu berikutnya
-                                        currentDay = day;
-                                    }
-                                }
+for (let i = 1; i <= 8; i++) {
+    const headerDate = document.getElementById(`day${i}`).children[1].textContent.trim();
+    const dateParts = headerDate.split(' ');
+    const day = parseInt(dateParts[0]);
+    const dayColumn = document.getElementById(`daydata${machine.machine_id}-${day}`);
+    if (dayColumn) {
+        const addButton = document.createElement('button');
+        addButton.className = 'add-jpm-button add-data-button rounded-full bg-grey-500 text-white text-xs w-7 h-7 thin-plus';
+        addButton.textContent = '+';
+        addButton.onclick = async function() {
+            const params = new URLSearchParams(window.location.search);
+            const line = params.get('line');
+            currentMachineId = machine.id;
+            currentMachineIdWeekly = machine.machine_id;
+            currentDay = day;
+            currentMonth = getMonthNumber(dateParts[1]);
+            currentYear = parseInt(dateParts[2]);
 
-                                addDataModal.classList.remove('hidden');
-                            };
-                            dayColumn.appendChild(addButton);
+            const isLastMonday = i === 8;
+            if (isLastMonday) {
+                const totalWeeks = getWeeksInMonth(currentYear, currentMonth);
+                let nextWeekUrl = '';
+                switch (parseInt(week)) {
+                    case 1:
+                        nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=2`;
+                        break;
+                    case 2:
+                        nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=3`;
+                        break;
+                    case 3:
+                        nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=4`;
+                        break;
+                    case 4:
+                        nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=5`;
+                        break;
+                    case 5:
+                        if (totalWeeks === 6) {
+                            nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${currentYear}&month=${currentMonth}&week=6`;
+                        } else {
+                            const nextMonth = (currentMonth % 12);
+                            const nextYear = nextMonth === 1 ? currentYear + 1 : currentYear;
+                            nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${nextYear}&month=${nextMonth}&week=2`;
                         }
-                    }
+                        break;
+                    case 6:
+                        const nextMonth = (currentMonth % 12);
+                        const nextYear = nextMonth === 1 ? currentYear + 1 : currentYear;
+                        nextWeekUrl = `http://127.0.0.1:8000/api/showweeklymachine?line=${line}&year=${nextYear}&month=${nextMonth}&week=2`;
+                        break;
+                    default:
+                        console.error('Invalid week number');
+                        return;
+                }
 
-                    machineRow.querySelector('.mesin-jpm').onclick = function() {
-                        viewMachineData(machine);
-                    };
-                });
+                const response = await fetch(nextWeekUrl);
+                const nextWeekMachines = await response.json();
+                const nextWeekMachine = nextWeekMachines.find(m => m.machine_name === machine.machine_name);
+                if (nextWeekMachine) {
+                    currentMachineIdWeekly = nextWeekMachine.machine_id;
+                    currentMachineId = nextWeekMachine.id;
+                    currentDay = day;
+                }
             }
+
+            addDataModal.classList.remove('hidden');
+        };
+        dayColumn.appendChild(addButton);
+    }
+}
+
+machineRow.querySelector('.mesin-jpm').onclick = function() {
+    viewMachineData(machine);
+}
+})}
 
 
             function showNotesPopup(event, notes) {
